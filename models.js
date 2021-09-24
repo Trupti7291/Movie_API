8d0bd334660101c4ca13f148d11bf188a0db30e0
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-09-24 16:01:40 +0200

    Update models.js
    updated Return data about a genre and director

--- models.js
+++ models.js
@@ -14,16 +14,32 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+let genreSchema = mongoose.Schema({
+    Name: { type: String, required: true },
+    Description: { type: String, required: true }
+});
+
+let directorSchema = mongoose.Schema({
+    Name: { type: String, required: true },
+    Bio: { type: String, required: true },
+    Birthdate: Date
+});
+
 let userSchema = mongoose.Schema({
     Username: { type: String, required: true },
     Password: { type: String, required: true },
     Email: { type: String, required: true },
-    Birthday: Date,
+    Birthdate: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
+
