c64511fb2e163f3a3f7e60c26b86e4caf37e5918
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-04 17:30:21 +0200

    Update index.js
    updated Mongoose connect()

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://myFlixDBadmin:KoYaxHLXNIB1RdTI@myappdb.r5gro.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
 const cors = require('cors');
 
 app.use(bodyParser.json());
