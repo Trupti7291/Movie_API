f7b51129c9786b22253facc1059e10cc67015179
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-04 17:43:13 +0200

    Update index.js
    updated mongoose connect with Environment Variables

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 
 // mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://myFlixDBadmin:KoYaxHLXNIB1RdTI@myappdb.r5gro.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
 
