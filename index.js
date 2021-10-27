362cf5bb22a370992d14cc78705eac8d7019f30c
Ghanshyam Hirani <87382991+Trupti7291@users.noreply.github.com>
2021-10-27 16:52:56 +0200

    Update index.js
    Deleted cors

--- index.js
+++ index.js
@@ -8,16 +8,7 @@ const { check, validationResult } = require('express-validator');
 
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'http://testsite.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 const app = express();
 const Movies = Models.Movie;
